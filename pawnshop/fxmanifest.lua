2bd5b36a31c6de782903ab364812b50d6ebebb9f
Techy Game Bar <65540686+techygamebar@users.noreply.github.com>
2021-04-25 17:19:08 +0530

    Create fxmanifest.lua

---
+++ pawnshop/fxmanifest.lua
@@ -0,0 +1,15 @@
+name "pawnshop"
+contact "discord.gg/hxeyBaR"
+
+fx_version "adamant"
+game "gta5"
+
+description "A simple pawnshop Made by LEGEND|TGB#1058"
+
+
+
+client_script "scripts/cl_*.lua"
+server_script "scripts/sv_*.lua"
+
+client_script "config.lua"
+server_script "config.lua"
