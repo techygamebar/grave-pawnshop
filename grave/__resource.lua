8140325493cf2631433c58e7503bd67e916c0a60
Techy Game Bar <65540686+techygamebar@users.noreply.github.com>
2021-04-25 17:16:40 +0530

    Create __resource.lua

---
+++ grave/__resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description "grave Script made by LEGEND|TGB#1058"
+
+author "LEGEND|TGB#1058"
+
+client_scripts {
+    "client.lua"
+}
+
+server_scripts {
+    "server.lua"
+}
