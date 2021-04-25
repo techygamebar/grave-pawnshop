9e52edd052cd9ffcd6feed0685fc0e6e14469882
Techy Game Bar <65540686+techygamebar@users.noreply.github.com>
2021-04-25 17:19:33 +0530

    Create config.lua

---
+++ pawnshop/config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+Config.PawnshopItems = {
+    oldshoe = 500,
+    oldring = 200,  
+    ceas = 50,
+}
+
+Config.GiveBlack = false 
+
+Config.PawnshopLocation =  {x = 182.07, y = -1319.06, z = 29.32}
+
+
+Config.PawnshopBlipText = "Pawnshop"
+Config.PawnshopBlipColor = 5
+Config.PawnshopBlipSprite = 272
+
+Config.EnableOpeningHours = true 
+Config.OpenHour = 9 
+Config.CloseHour = 18 
